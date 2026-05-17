bb217ffe1ec02e2add76b19b63b9be5446228654
Eduardo Ramos <otr07eduardo@gmail.com>
2026-05-17 16:13:38 -0300

    Create 503_nextGreaterElements.c

---
+++ AED1/ExerciciosPraticos/Exercicio2/503_nextGreaterElements.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* NextGreaterElements( int* nums, int numsSize, int* returnSize ) {
+
+    int* greaterNums = ( int* )malloc( sizeof( int ) * numsSize );
+    int i = 0;
+    *returnSize = numsSize;
+
+    while ( i < numsSize ) {
+        greaterNums[i] = -1;
+        int j = 1;
+        while ( j < numsSize ) {
+            int circular = ( i + j ) % numsSize;
+            if ( nums[circular] > nums[i] ) {
+                greaterNums[i] = nums[circular];
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    return greaterNums;
+}
+
+int main( void ) {
+    int nums[] = {1,5,3,1,2}, returnSize;
+    int numsSize = sizeof( nums ) / sizeof( nums[0] );
+
+    int *result = NextGreaterElements( nums, numsSize, &returnSize );
+
+    printf( "[" );
+
+    for ( int i = 0; i < returnSize; i++ ) {
+        printf( "%d", result[i] );
+
+        if ( i < returnSize - 1 ) {
+            printf( "," );
+        }
+    }
+
+    printf( "]" );
+
+    free( result );
+
+    return 0;
+}
