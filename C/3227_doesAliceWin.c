3827d5947483fc782ebd81c03deaa6a38a1a8d7f
Eduardo Ramos <otr07eduardo@gmail.com>
2026-07-04 02:06:03 -0300

    Create 3227_doesAliceWin.c

---
+++ C/3227_doesAliceWin.c
@@ -0,0 +1,8 @@
+bool doesAliceWin(char* s) {
+
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') return true;
+    }
+
+    return false;
+}
